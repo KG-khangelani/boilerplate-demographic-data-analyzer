cd6aaa6e1fa1e1a96b9df469793a220e66db9cd5
kaygizzle Khangelani <mgoqikhangelani@gmail.com>
2024-02-18 17:33:19 +0000

    demographic exercise

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,44 +1,47 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df["sex"] == "Male"]["age"].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    num_bachelors = len(df[df["education"] == "Bachelors"])
+    total_num = len(df)
+    percentage_bachelors = round((num_bachelors / total_num) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df[df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
+    lower_education = df[~df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round(len(higher_education[higher_education.salary == ">50K"]) / len(higher_education) * 100, 1)
+    lower_education_rich = round(len(lower_education[lower_education.salary == ">50K"]) / len(lower_education) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
-
-    rich_percentage = None
+    num_min_workers = df[df["hours-per-week"] == min_work_hours]
+    rich_percentage = round(len(num_min_workers[num_min_workers.salary == ">50K"]) / len(num_min_workers) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    country_count = df["native-country"].value_counts()
+    rich_country_count = df[df.salary == ">50K"]["native-country"].value_counts()
+    highest_earning_country = (rich_country_count / country_count).idxmax()
+    highest_earning_country_percentage = round((rich_country_count / country_count * 100).max(), 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    occupation_count = df[(df["native-country"] == "India") & (df["salary"] == ">50K")]["occupation"].value_counts()
+    top_in_occupation = occupation_count.idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -52,7 +55,7 @@ def calculate_demographic_data(print_data=True):
         print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
         print("Country with highest percentage of rich:", highest_earning_country)
         print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
-        print("Top occupations in India:", top_IN_occupation)
+        print("Top occupations in India:", top_in_occupation)
 
     return {
         'race_count': race_count,
@@ -63,7 +66,6 @@ def calculate_demographic_data(print_data=True):
         'min_work_hours': min_work_hours,
         'rich_percentage': rich_percentage,
         'highest_earning_country': highest_earning_country,
-        'highest_earning_country_percentage':
-        highest_earning_country_percentage,
-        'top_IN_occupation': top_IN_occupation
+        'highest_earning_country_percentage': highest_earning_country_percentage,
+        'top_IN_occupation': top_in_occupation
     }
